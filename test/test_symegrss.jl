906dd826978713c9c3e13b7ceb2eb7b270221926
Mikkel Paltorp Schmitt <mikkel.paltorp@gmail.com>
2019-10-24 19:47:08 +0200

    Commenting tests

--- test/test_symegrss.jl
+++ test/test_symegrss.jl
@@ -1,3 +1,4 @@
+# Creating a test problem
 n = 100;
 p = 5;
 U = randn(n,p);
@@ -6,5 +7,7 @@ K = SymEGRSSMatrix(U,V);
 x = randn(n);
 Kfull = tril(U*V') + triu(V*U',1);
 
+# Testing multiplication
 @test isapprox(K*x, Kfull*x, atol = 1e-6)
+#  Testing multiplication with the adjoint operator
 @test isapprox(K'*x, Kfull'*x, atol = 1e-6)
